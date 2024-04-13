8548d0ab8bd227ea9c5a94a8469a5e956078b9a1
Роман <rkaeplive@gmail.com>
2024-04-13 20:30:12 +0300

    v1.1.0

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ viewer.utils.useStats = true;
 document.addEventListener("DOMContentLoaded", () => {
     viewer.utils.propsUtils.initPropConteiner(document.getElementById("props"));
     window.addEventListener("keydown", onKeyDown);
+    const input = document.getElementById("file-input");
+    const demoIfc = document.getElementById("demo-ifc");
+    const demoBmt = document.getElementById("demo-bmt");
+    const exportBmt = document.getElementById("export-bmt");
 
+    const color = document.getElementById("color");
+    const colorize = document.getElementById("colorize");
     const infoBut = document.getElementById("infoBut");
     const info = document.getElementById("info");
     infoBut.addEventListener("mouseenter", () => {
@@ -15,10 +21,67 @@ document.addEventListener("DOMContentLoaded", () => {
     infoBut.addEventListener("mouseleave", () => {
         info.style.display = "none";
     });
-    viewer.loadModel("/bimatter-viewer-demo/model.bmt", true).then((model) => {
-        console.log(model);
-        console.log(viewer);
+    var curColor = "#454545";
+    colorize.addEventListener("click", () => {
+        viewer.utils.geometryUtils.createGeometryChunk({
+            modelID: 0,
+            ids: Array.from(viewer.selector.selectedElements[0]),
+            color: curColor,
+        });
     });
+    color.addEventListener("change", (e) => {
+        curColor = e.target.value;
+    });
+    input.addEventListener(
+        "change",
+        (changed) => {
+            const file = changed.target.files[0];
+            viewer.loadModel(file, true).then((model) => {
+                console.log(model);
+                console.log(viewer);
+            });
+            demoIfc.remove();
+            demoBmt.remove();
+            exportBmt.style.display = "block";
+            input.remove();
+        },
+        false
+    );
+    demoIfc.addEventListener("click", () => {
+        viewer
+            .loadModel("/bimatter-viewer-demo/model.ifc", true)
+            .then((model) => {
+                console.log(model);
+                console.log(viewer);
+            });
+        demoIfc.remove();
+        demoBmt.remove();
+        exportBmt.style.display = "block";
+        input.remove();
+    });
+    demoBmt.addEventListener("click", () => {
+        viewer
+            .loadModel("/bimatter-viewer-demo/model.bmt", true)
+            .then((model) => {
+                console.log(model);
+                console.log(viewer);
+            });
+        // viewer.loadModel("/model.bmt", true).then((model) => {
+        //     console.log(model);
+        //     console.log(viewer);
+        // });
+        demoIfc.remove();
+        demoBmt.remove();
+        exportBmt.style.display = "block";
+        input.remove();
+    });
+    exportBmt.addEventListener("click", () => {
+        if (viewer && viewer.models[0]) {
+            viewer.utils.exportUtils.exportBMT(0);
+        }
+    });
+    // demoIfc.click();
+    // demoBmt.click();
 });
 
 // viewer.loadModel("/model.bmt", true).then((model) => {
